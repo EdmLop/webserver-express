6f8402de8ef6e698ad7a013bae07f4eace644d4e
Edmundo <correoedmundo@gmail.com>
2020-11-10 13:03:55 -0600

    Primer commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+//HBS
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/parciales');
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'ricardo edmundo',
+        anio: new Date().getFullYear()
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        anio: new Date().getFullYear()
+    });
+});
+
+/* app.get('/data', (req, res) => {
+    res.send('Hola data');
+});
+ */
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+});
